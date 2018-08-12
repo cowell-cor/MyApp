0e864feae12598b468379f18c9f9b6fe33cf11e5
Cowell Cor <cowell.cor@foresee.com>
2018-08-11 18:59:15 -0700

    Added hoghchart initial version

--- app/src/app/hisaCalculator/scenarioResults/hisaScenarioResults.js
+++ app/src/app/hisaCalculator/scenarioResults/hisaScenarioResults.js
@@ -1,10 +1,42 @@
 (function($,jQuery){
-brCalc.controller('retirementSavingsScenarioResultsCtrl', ["$scope","$attrs","scenarios",function($scope,$attrs,scenarios) {
-	var scenario = scenarios.getScenario('retirementSavingsData',$attrs.scenarioIndex),
+brCalc.controller('hisaScenarioResultsCtrl', ["$scope","$attrs","scenarios","contentManager",function($scope,$attrs,scenarios,contentManager) {
+	var scenario = scenarios.getScenario('hisaData',$attrs.scenarioIndex,contentManager),
 		me = this;
 
 	this.data = scenario.data;
 	this.data.scenarioIndex = $attrs.scenarioIndex;
 	this.results = scenario.results;
+
+
+	//me.results.chartHISA = contentManager.getHighchartConfig('chartHISA');
+	initChart();
+
+	function initChart() {
+		me.results.chartHISA = (function(){
+
+			var config = contentManager.getHighchartConfig('chartHISA');
+			// Tooltip formatting
+			config.tooltip.formatter = function(){
+				var sum = 0,s;
+				$.each(this.points, function () {
+				sum+= this.y;
+				});
+				s = '<span>After ' + this.x + ' you will have:</span><br/><b>Total Savings $' + sum;
+				$.each(this.points, function () {
+					me.results.seriesName = this.series.name;
+					console.log(this);
+					s += '<br/>' + this.series.name + ': $' +
+						this.y;
+					sum+= this.y;
+				
+				});
+				me.results.total = sum;
+
+				return s;
+			};
+			return config;
+		})();
+		
+	}
 }]);
 })($cmsj,$cmsj);
