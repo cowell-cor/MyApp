573f90bd85b47263e5af7390cd45f17d83372c50
cowell-cor <cowellkour@gmail.com>
2018-08-14 16:22:51 -0700

    making clear action working

--- app/src/app/hisaCalculator/scenario/hisaScenario.js
+++ app/src/app/hisaCalculator/scenario/hisaScenario.js
@@ -1,18 +1,17 @@
-(function($,jQuery){
-brCalc.controller('hisaScenarioCtrl', function($scope,$attrs,scenarios,$filter,contentManager) {
+(function ($, jQuery) {
+	brCalc.controller('hisaScenarioCtrl', function ($scope, $attrs, scenarios, $filter, contentManager, $rootScope) {
 		var me = this,
 			hisa = scenarios.getScenarios('hisaData'),
 			scenario = hisa.getScenario($attrs.scenarioIndex),
 			rscData = hisa.data,
 			content = $scope.hisa.content,
-			constants = $scope.hisa.data.constants,
-			currentYear = new Date().getFullYear();
+			constants = $scope.hisa.data.constants;
 
 		$scope.collapse = {
-			savings:false,
-			debitTransfer:true,
-			depositTransfer:true
-		};	
+			savings: false,
+			debitTransfer: true,
+			depositTransfer: true
+		};
 
 		//////////////////////////////
 		// View accessible variable //
@@ -24,19 +23,48 @@ brCalc.controller('hisaScenarioCtrl', function($scope,$attrs,scenarios,$filter,c
 
 		this.validation = scenario.validation;
 
+		/**
+		 * Function: resetDepositTransfer
+		 */
+
+		$scope.resetDebitTransfer = function () {
+			$scope.sce.data.numberOfMonthlyDebitTransactions = 0
+			$scope.hisa.specs.sliderDebitTransfer.defaultValue = 2
+			$rootScope.$broadcast('resetEvent', {
+				sliderId: 'debitTransfer_slider',
+				defaultVal: 2,
+				min:0,
+				max:5
+			});
+		}
+
+				/**
+		 * Function: resetDepositTransfer
+		 */
+
+		$scope.resetDepositTransfer = function () {
+			$scope.sce.data.monthlyCreditsPay = 0
+			$scope.hisa.specs.sliderDepositTransfer.defaultValue = 0
+			$rootScope.$broadcast('resetEvent', {
+				sliderId: 'depositTransfer_slider',
+				defaultVal: 0,
+				min:0,
+				max:100
+			});
+		}
 		///////////////////////////////
 		// Before watches initiation //
 		///////////////////////////////
 		initChart();
 
-//////////////////////////////
-// FIN FONCTIONS DE CALCULS //
-//////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////
+		//////////////////////////////
+		// FIN FONCTIONS DE CALCULS //
+		//////////////////////////////
+		////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 		function initChart() {
-			me.results.chartHISA = (function(){
+			me.results.chartHISA = (function () {
 
 				var config = contentManager.getHighchartConfig('chartHISA');
 
@@ -44,4 +72,4 @@ brCalc.controller('hisaScenarioCtrl', function($scope,$attrs,scenarios,$filter,c
 			})();
 		}
 	});
-})($cmsj,$cmsj);
+})($cmsj, $cmsj);
